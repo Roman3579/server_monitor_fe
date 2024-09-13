5f2599258be822f2e33c827884b606ba7b030ce8
RomanGlvac <romanglvac@gmail.com>
2024-09-13 15:08:49 +0200

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InfoEditComponent } from './views/info-edit/info-edit.component';
+import { AppsOverviewComponent } from './views/apps-overview/apps-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppsOverviewComponent,
+  },
+  {
+    path: 'edit/:target',
+    component: InfoEditComponent,
+  },
+];
