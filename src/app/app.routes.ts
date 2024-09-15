5f77ae5f599467eccadf88e7e062ba0009bdf6ac
RomanGlvac <romanglvac@gmail.com>
2024-09-15 08:47:51 +0200

    create app detail component & add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { InfoEditComponent } from './views/info-edit/info-edit.component';
 import { AppsOverviewComponent } from './views/apps-overview/apps-overview.component';
+import { AppDetailComponent } from './views/app-detail/app-detail.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'edit/:target',
     component: InfoEditComponent,
+  },
+  {
+    path: 'detail/:target',
+    component: AppDetailComponent,
   },
 ];
---
+++ src/app/views/app-detail/app-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-detail.component.html',
+  styleUrl: './app-detail.component.scss'
+})
+export class AppDetailComponent {
+
+}
