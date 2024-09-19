3c74b9bb5f347ca05b91b5560276cc0a14744fb0
RomanGlvac <romanglvac@gmail.com>
2024-09-19 18:38:24 +0200

    implement basic app info view

--- src/app/views/app-detail/app-detail.component.ts
+++ src/app/views/app-detail/app-detail.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input } from '@angular/core';
+import { ApiCallResult } from '../../models/api-call-result';
+import { AppInfoService } from '../../services/app-info.service';
+import { JsonPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-app-detail',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe, MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './app-detail.component.html',
-  styleUrl: './app-detail.component.scss'
+  styleUrl: './app-detail.component.scss',
 })
 export class AppDetailComponent {
+  private appInfoService = inject(AppInfoService);
+  private destroyRef = inject(DestroyRef);
+  target = input.required<string>();
+  targetOrigin = computed(() =>
+    this.appInfoService.extractOrigin(this.target())
+  );
+  appInfo?: ApiCallResult;
 
+  constructor() {
+    const dataSubscription = this.appInfoService.appInfoData.subscribe({
+      next: (data) => {
+        this.appInfo = data.apiCallResults.find(
+          (element) => element.targetUrl === this.target()
+        );
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      dataSubscription.unsubscribe();
+    });
+  }
 }
