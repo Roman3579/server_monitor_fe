b22c6b6837d25e563aa7eda1448eafa54ebd08e2
glvac <roman.glvac@union.sk>
2024-09-20 11:53:21 +0200

    make app status icon utilize new error info

--- src/app/views/ip-overview/ip-overview.component.ts
+++ src/app/views/ip-overview/ip-overview.component.ts
@@ -1,34 +1,28 @@
-import { NgClass } from '@angular/common';
-import {
-  AfterViewInit,
-  Component,
-  Input,
-  input,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterLink } from '@angular/router';
-import { ApiCallResult } from '../../models/api-call-result';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { FormsModule } from '@angular/forms';
-import { AppInfoService } from '../../services/app-info.service';
-import { saveAs } from 'file-saver';
-import { MatDialog } from '@angular/material/dialog';
-import { LoadingDialogComponent } from '../shared/loading-dialog/loading-dialog.component';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { showComingSoonMessage } from '../../services/coming-soon';
-import { LocalStorageService } from '../../services/local-storage.service';
+import {NgClass} from '@angular/common';
+import {AfterViewInit, Component, Input, input, OnInit, ViewChild,} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {RouterLink} from '@angular/router';
+import {ApiCallResult} from '../../models/api-call-result';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {FormsModule} from '@angular/forms';
+import {AppInfoService} from '../../services/app-info.service';
+import {saveAs} from 'file-saver';
+import {MatDialog} from '@angular/material/dialog';
+import {LoadingDialogComponent} from '../shared/loading-dialog/loading-dialog.component';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {LocalStorageService} from '../../services/local-storage.service';
+import {ApiCallError} from "../../models/api-call-error";
+import {ArgumentOutOfRangeError} from "rxjs";
 
 interface FlattenedApiCallResult {
   targetUrl: string;
   appType: string;
   description: string;
-  active: boolean;
+  error: ApiCallError;
   frontendUrl: string;
 }
 
@@ -38,7 +32,7 @@ function flattenResults(results: ApiCallResult[]) {
       targetUrl: result.targetUrl,
       appType: result.appInfoModel.appType,
       description: result.appInfoModel.description,
-      active: result.appInfoModel.appType !== 'error',
+      error: result.appInfoModel.apiCallError,
       frontendUrl: result.appInfoModel.frontendUrl,
     };
   });
@@ -123,7 +117,20 @@ export class IpOverviewComponent implements OnInit, AfterViewInit {
     });
   }
 
-  protected readonly showComingSoonMessage = showComingSoonMessage;
+  getStatusColor(errorValue?: ApiCallError) {
+    switch (errorValue){
+      case null:
+        return "success"
+      case ApiCallError.NOT_FOUND:
+        return "accent"
+      case ApiCallError.CONNECTION_ERROR:
+        return "warn"
+      case ApiCallError.UNKNOWN:
+        return "primary"
+      default:
+        throw new ArgumentOutOfRangeError()
+    }
+  }
 
   toggleExpansion() {
     this.expanded = !this.expanded;
