a3816d4729b91ec3330315d99b92f25dffdc5515
glvac <roman.glvac@union.sk>
2024-09-20 11:51:16 +0200

    include ApiCallError in AppInfo & create separate AppInfoRequest model

--- src/app/models/app-info.ts
+++ src/app/models/app-info.ts
@@ -1,4 +1,13 @@
+import {ApiCallError} from "./api-call-error";
+
 export interface AppInfo {
+  appType: string;
+  description: string;
+  frontendUrl: string;
+  apiCallError: ApiCallError;
+}
+
+export interface AppInfoRequest {
   appType: string;
   description: string;
   frontendUrl: string;
