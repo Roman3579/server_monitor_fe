13bceada2f33a505ac8717ffd3d911168e6577ec
RomanGlvac <romanglvac@gmail.com>
2024-09-21 12:26:33 +0200

    change response processing to accommodate for API change

--- src/app/models/app-info.ts
+++ src/app/models/app-info.ts
@@ -1,10 +1,7 @@
-import {ApiCallError} from "./api-call-error";
-
 export interface AppInfo {
   appType: string;
   description: string;
   frontendUrl: string;
-  apiCallError: ApiCallError;
 }
 
 export interface AppInfoRequest {
