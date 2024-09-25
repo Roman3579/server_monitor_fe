dda1927e92d3902fb17c4e3e05fc4e521c71b803
Smincak <lukas.smincak@union.sk>
2024-09-25 12:01:27 +0200

    add content for app-detail

--- src/app/views/app-detail/app-detail.component.ts
+++ src/app/views/app-detail/app-detail.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, computed, input} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
+import {MatIconButton} from "@angular/material/button";
+import {MatTooltip} from "@angular/material/tooltip";
+import {AppInfoService} from "../../services/app-info.service";
+
 
 @Component({
   selector: 'app-app-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    RouterLink,
+    MatIconButton,
+    MatTooltip
+  ],
   templateUrl: './app-detail.component.html',
   styleUrl: './app-detail.component.scss'
 })
 export class AppDetailComponent {
+  target = input.required<string>();
+  originIp = computed(() => {
+    try {
+      return this.appInfoService.extractIp(this.target());
+    } catch (error) {
+      console.warn(`Failed to extract Ip from ${this.target()}`);
+      return 'unknown';
+    }
+  });
 
+  originPort = computed(() => {
+    try {
+      return this.appInfoService.extractPort(this.target());
+    } catch (error) {
+      console.warn(`Failed to extract port from ${this.target()}`);
+      return 'unknown';
+    }
+  });
+
+  originAppInfo = computed(() => {
+    try {
+      return this.appInfoService.getAppInfo();
+    } catch (error) {
+      console.warn(`Failed to extract info from ${this.target()}`);
+      return 'unknown';
+    }
+  });
+  constructor(
+    private appInfoService: AppInfoService
+  ) {}
 }
+
