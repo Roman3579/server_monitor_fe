13ce71ec1a12d08d1b255cf0a70a0bc0ffb04edc
RomanGlvac <romanglvac@gmail.com>
2024-09-25 18:14:55 +0200

    register api config provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,17 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { API_CONFIG_TOKEN, apiConfig } from './config/api-config';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
     provideAnimationsAsync(),
+    {
+      provide: API_CONFIG_TOKEN,
+      useValue: apiConfig,
+    },
   ],
 };
