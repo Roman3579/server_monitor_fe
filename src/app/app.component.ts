17bd1d7065173c9c2b93b2d38356ea2d27ff44e2
RomanGlvac <romanglvac@gmail.com>
2024-10-02 19:22:19 +0200

    add navigation component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppsOverviewComponent } from './views/apps-overview/apps-overview.component';
+import { LoadingDialogComponent } from './views/shared/loading-dialog/loading-dialog.component';
+import { NavigationComponent } from './views/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppsOverviewComponent],
+  imports: [
+    RouterOutlet,
+    AppsOverviewComponent,
+    LoadingDialogComponent,
+    NavigationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
